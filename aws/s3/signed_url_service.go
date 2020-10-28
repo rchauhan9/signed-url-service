122cef3c7c125c265bbbbeb16ae2053d5a457689
Rajan <rajan.chauhan18@imperial.ac.uk>
2020-10-28 11:13:44 +0000

    Using HeadObject to determine if object exists or not so can send 200 or 404 accordingly.

--- aws/s3/signed_url_service.go
+++ aws/s3/signed_url_service.go
@@ -1,6 +1,8 @@
 package s3
 
 import (
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -8,13 +10,27 @@ import (
 	"time"
 )
 
-func GenerateSignedURL(bucket string, key string) (string) {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("eu-west-2")},
-	)
+func GenerateSignedURL(bucket string, key string) (string, error) {
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("eu-west-2")})
 
 	// Create S3 service client
 	svc := s3.New(sess)
+
+	output, err := svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+
+	fmt.Print(output.LastModified)
+	if output.LastModified == nil {
+		return "", errors.New(fmt.Sprintf("object %s/%s not found", bucket, key))
+	}
+
 	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
@@ -25,5 +41,5 @@ func GenerateSignedURL(bucket string, key string) (string) {
 		log.Println("Failed to sign request", err)
 	}
 
-	return urlStr
+	return urlStr, nil
 }
