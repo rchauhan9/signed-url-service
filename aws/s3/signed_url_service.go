2947f3b10f5319a45a7e8cae3e337d7d81710136
Rajan <rajan.chauhan18@imperial.ac.uk>
2020-10-30 15:13:45 +0000

    Using HeadObjectRequest to determine if object exists or not and refactored this logic into a separate method.

--- aws/s3/signed_url_service.go
+++ aws/s3/signed_url_service.go
@@ -6,40 +6,48 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"log"
 	"time"
 )
 
 func GenerateSignedURL(bucket string, key string) (string, error) {
 
 	sess, err := session.NewSession(&aws.Config{Region: aws.String("eu-west-2")})
-
-	// Create S3 service client
+	if err != nil {
+		return "", err
+	}
 	svc := s3.New(sess)
 
-	output, err := svc.HeadObject(&s3.HeadObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-	})
-
+	err = verifyObjectExists(svc, bucket, key)
 	if err != nil {
-		fmt.Errorf(err.Error())
-	}
-
-	fmt.Print(output.LastModified)
-	if output.LastModified == nil {
-		return "", errors.New(fmt.Sprintf("object %s/%s not found", bucket, key))
+		return "", err
 	}
 
 	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
-	urlStr, err := req.Presign(15 * time.Minute)
 
+	urlStr, err := req.Presign(15 * time.Minute)
 	if err != nil {
-		log.Println("Failed to sign request", err)
+		return "", err
 	}
 
 	return urlStr, nil
+}
+
+func verifyObjectExists(svc *s3.S3, bucket string, key string) error {
+	request, output := svc.HeadObjectRequest(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	err := request.Send()
+	if err != nil {
+		return err
+	}
+	if output.LastModified == nil {
+		return errors.New(fmt.Sprintf("object %s/%s not found", bucket, key))
+	}
+
+	return nil
 }
